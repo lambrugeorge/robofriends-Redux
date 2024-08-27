5ca7f195b677677ec0ad63ae5ecb1a7695fb96e4
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-08-27 23:56:22 +0300

    first commit with Provider , Connect from Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 
-
+const store = createStore(searchRobots);
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
