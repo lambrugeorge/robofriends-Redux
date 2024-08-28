862aba86071d2fdb26b88f775146f9cfbdaab9b7
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-08-28 22:46:30 +0300

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+const logger = createLogger();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
