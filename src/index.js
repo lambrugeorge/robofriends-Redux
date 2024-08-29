91b924816d01f38416c3b8412f37b72379825841
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-08-29 16:15:12 +0300

    creating new reductors and actions (Pending, success, failed)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import './index.css'; 
+import { thunk } from 'redux-thunk';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots, applyMiddleware(logger));
-
+// Create the logger middleware
 const logger = createLogger();
 
+// Create the Redux store with the logger middleware
+const store = createStore(
+  searchRobots,
+  applyMiddleware(thunk, logger)
+);
+
+// Render the React app
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
