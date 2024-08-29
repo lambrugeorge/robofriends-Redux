1cee21a9a17f78f9528b994c5dacb42add82c1d1
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-08-29 16:54:04 +0300

    redux async actions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import './index.css'; 
 import { thunk } from 'redux-thunk';
 import App from './containers/App';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 // Create the logger middleware
 const logger = createLogger();
 
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+
 // Create the Redux store with the logger middleware
-const store = createStore(
-  searchRobots,
-  applyMiddleware(thunk, logger)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 
 // Render the React app
 const root = createRoot(document.getElementById('root'));
